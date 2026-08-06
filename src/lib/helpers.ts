d5adffb23e525f35ce2d2d89e5503f6191b2721f
DuCJ-creator <DuCJ@chhs.hcc.edu.tw>
2026-08-06 11:07:57 +0800

    Add files via upload

---
+++ src/lib/helpers.ts
@@ -0,0 +1,15 @@
+export const hashString = (s: string) => {
+  let hash = 0;
+  for (let i = 0; i < s.length; i++) {
+    hash = ((hash << 5) - hash) + s.charCodeAt(i);
+    hash |= 0;
+  }
+  return Math.abs(hash);
+};
+
+export const getLocalDateString = (date: Date = new Date()) => {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  return `${year}-${month}-${day}`;
+};
